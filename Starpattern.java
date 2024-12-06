339e1be80f35525e5b0d6e66bfc32bc24679a96a
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-06 13:43:01 +0530

    Add files via upload

---
+++ Starpattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Starpattern{
+    public static void main(String args[]){
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=i;j<=5;j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+
+        }
+    }        
+}
